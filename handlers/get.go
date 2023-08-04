72e97af23dfbc63541991313320dd83ba6029437
Jbfaneto <jbfaneto@gmail.com>
2023-08-04 15:16:14 -0300

    first commit

---
+++ handlers/get.go
@@ -0,0 +1,30 @@
+package handlers
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/Jbfaneto/api-go/models"
+	"github.com/go-chi/chi/v5"
+)
+
+func Get(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		log.Printf("Error ao fazer parse do ID: %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	todo, err := models.Get(int64(id))
+	if err != nil {
+		log.Printf("Error ao procurar registro: %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(todo)
+}
---
+++ handlers/update.go
@@ -0,0 +1,47 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/Jbfaneto/api-go/models"
+	"github.com/go-chi/chi/v5"
+)
+
+func Update(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		log.Printf("Error ao fazer parse do ID: %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	var todo models.Todo
+	err = json.NewDecoder(r.Body).Decode(&todo)
+	if err != nil {
+		log.Printf("Erro ao fazer decode do json: %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+	}
+
+	rows, err := models.Update(int64(id), todo)
+	if err != nil {
+		log.Printf("Error ao atualizar registro: %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	if rows > 1 {
+		log.Printf("Error: Foram atualizados %d registros", rows)
+	}
+
+	resp := map[string]any{
+		"Error":   false,
+		"Message": fmt.Sprintf("Todo atualizado com sucesso! ID: %d", id),
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(resp)
+}
